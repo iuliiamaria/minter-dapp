a48634630c017205aa2ecc8d20bae73815d60404
Iulia Rauta <rauta.iuliia@gmail.com>
2022-03-21 17:41:49 +0200

    update

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -653,6 +653,19 @@ const abi = [
   },
   {
     inputs: [],
+    name: "presaleMintPrice",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
     name: "mintingActive",
     outputs: [
       {
