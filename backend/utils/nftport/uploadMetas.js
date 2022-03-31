70a90a8932bfc6a6387d224ec92e29c736078cee
Iulia Rauta <rauta.iuliia@gmail.com>
2022-03-31 18:13:24 +0300

    add new contract

--- backend/utils/nftport/uploadMetas.js
+++ backend/utils/nftport/uploadMetas.js
@@ -17,36 +17,36 @@ let readDir = `${basePath}/build/json`;
 let writeDir = `${basePath}/build/ipfsMetas`;
 
 async function main() {
-  console.log(`Starting upload of metadata...`);
-  const files = fs.readdirSync(readDir);
-  files.sort(function (a, b) {
-    return a.split(".")[0] - b.split(".")[0];
-  });
-  const formData = new FormData();
+  // console.log(`Starting upload of metadata...`);
+  // const files = fs.readdirSync(readDir);
+  // files.sort(function (a, b) {
+  //   return a.split(".")[0] - b.split(".")[0];
+  // });
+  // const formData = new FormData();
 
-  for (const file of files) {
-    if (regex.test(file)) {
-      const fileStream = fs.createReadStream(`${readDir}/${file}`);
-      formData.append("metadata_files", fileStream);
-    }
-  }
+  // for (const file of files) {
+  //   if (regex.test(file)) {
+  //     const fileStream = fs.createReadStream(`${readDir}/${file}`);
+  //     formData.append("metadata_files", fileStream);
+  //   }
+  // }
 
-  try {
-    const url = "https://api.nftport.xyz/v0/metadata/directory";
-    const options = {
-      method: "POST",
-      headers: {},
-      body: formData,
-    };
-    const response = await fetchWithRetry(url, options);
-    fs.writeFileSync(
-      `${writeDir}/_ipfsMetasResponse.json`,
-      JSON.stringify(response, null, 2)
-    );
-    console.log(`Metadata uploaded!`);
-  } catch (err) {
-    console.log(`Catch: ${err}`);
-  }
+  // try {
+  //   const url = "https://api.nftport.xyz/v0/metadata/directory";
+  //   const options = {
+  //     method: "POST",
+  //     headers: {},
+  //     body: formData,
+  //   };
+  //   const response = await fetchWithRetry(url, options);
+  //   fs.writeFileSync(
+  //     `${writeDir}/_ipfsMetasResponse.json`,
+  //     JSON.stringify(response, null, 2)
+  //   );
+  //   console.log(`Metadata uploaded!`);
+  // } catch (err) {
+  //   console.log(`Catch: ${err}`);
+  // }
 
   // Upload Generic Metadata if GENERIC is true
   if (GENERIC) {
@@ -64,12 +64,10 @@ async function main() {
     const genericObject = {
       "name": metaData.name,
       "description": metaData.description,
-      "file_url": metaData.image,
+      "image": metaData.image,
+      "date": metaData.date,
+      "creator": metaData.creator,
       "external_url": metaData?.external_url,
-      "custom_fields": {
-        "date": metaData.date,
-        "compiler": "HashLips Art Engine - codeSTACKr Modified"
-      }
     }
 
     try {
--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,4 +1,4 @@
-const contractAddress = "0x380e47472a7c572285a138b734ff855cc83d6196"; //Replace with your own contract address
+const contractAddress = "0x23b2FF945781a20AA9Da14c03A193e8E35505e0D"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the LowKey The $cottish NFT Project!!";
